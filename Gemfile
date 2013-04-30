a758548eb5e5e77a4dbd67a05a28bf42ca36ac7d
Sho Hashimoto <hashimoto@shokai.org>
2013-04-30 16:15:40 +0900

    display weather with LED

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+gem 'arduino_firmata'
+gem 'weather_jp'
---
+++ teruteru.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'arduino_firmata'
+require 'weather_jp'
+
+if ARGV.empty?
+  STDERR.puts "ruby #{$0} yokohama"
+  exit 1
+end
+weather = WeatherJp.get ARGV.shift
+puts weather.today
+arduino = ArduinoFirmata.connect ARGV.shift
+
+LED_RED   = 11
+LED_GREEN = 12
+LED_BLUE  = 13
+
+if weather.today.rain.to_i > 30
+  arduino.digital_write LED_BLUE, true
+else
+  arduino.digital_write LED_RED, true
+end
